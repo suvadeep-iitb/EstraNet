bfc84bce82d6dd0f2fcb0317d272cc47ce8bc184
Suvadeep <suvadeep.hajra@gmail.com>
2024-10-07 15:32:19 +0530

    Updated for compatibility to newer version of tensorflow

--- train_trans.py
+++ train_trans.py
@@ -453,7 +453,7 @@ def main(unused_argv):
         assert False
 
     if FLAGS.use_tpu:
-        resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='grpc://' + os.environ['COLAB_TPU_ADDR'])
+        resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
         tf.config.experimental_connect_to_cluster(resolver)
         tf.tpu.experimental.initialize_tpu_system(resolver)
         strategy = tf.distribute.experimental.TPUStrategy(resolver)
